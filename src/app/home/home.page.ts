65b1f7147e8a7b50e28ce7736a01a67573acff6d
Niklas Merz <nmerz@gedys-intraware.de>
2021-01-15 15:26:17 +0100

    Basic reproduction

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  fields = [];
 
-  constructor() {}
+  constructor(private loadingCtrl: LoadingController) {
+    this.buildList();
+  }
 
+  buildList() {
+    this.fields = [];
+    for (let i = 0; i < 20; i++) {
+      this.fields.push('Field ' + i);
+    }
+  }
+
+  async refresh() {
+    for (let i = 0; i < 20; i++) {
+      this.fields[i] = 'Field Refreshed' + i;
+    }
+  }
 }
